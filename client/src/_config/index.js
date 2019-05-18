9e5d2ff6594e0b8b2924a326c6cbc0d2ffad5813
Anthony Ngene <ngeneanthony@gmail.com>
2019-05-18 15:24:17 +0100

    use correct url

--- client/src/_config/index.js
+++ client/src/_config/index.js
@@ -1,6 +1,7 @@
-let root = "";
-if (process.env.NODE_ENV === "development") root = "http://127.0.0.1:8000";
+let rootUrl = "https://api.mentorci.ga/api/v1/";
+if (process.env.NODE_ENV === "development")
+  rootUrl = "http://127.0.0.1:8000/api/v1/";
 
 export default {
-  apiUrl: root + "http://35.181.102.147/api/v1/"
+  apiUrl: rootUrl
 };
