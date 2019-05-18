668705e4ef985750bbcb478a7f4caf84565b5bab
Anthony Ngene <ngeneanthony@gmail.com>
2019-05-18 13:51:24 +0100

    configure api url (aws elastic ip - ec2)

--- client/src/_config/index.js
+++ client/src/_config/index.js
@@ -2,5 +2,5 @@ let root = "";
 if (process.env.NODE_ENV === "development") root = "http://127.0.0.1:8000";
 
 export default {
-  apiUrl: root + "/api/v1/"
+  apiUrl: root + "http://35.181.102.147/api/v1/"
 };
